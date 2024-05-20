12cb0e8491fe6e52109069ff5cf6b28555f85466
Natanaelsl <natanaelsoaresleite@gmail.com>
2024-05-19 22:59:31 -0300

    up

---
+++ tests/testthat/tests.R
@@ -0,0 +1,31 @@
+test_that("check conection", {
+
+  testthat::expect_true(RCurl::url.exists("https://apidatalake.tesouro.gov.br/docs/siconfi/"))
+
+})
+
+
+test_that("check data.frame", {
+
+  anexo1 <- RREORGFdataR::RGFdata(cod.ibge = 52, ano = 2023, poder = 'E', periodo = 1, anexo = 1, simplificado = FALSE)
+  anexo2 <- RREORGFdataR::RGFdata(cod.ibge = 52, ano = 2023, poder = 'E', periodo = 2, anexo = 1, simplificado = FALSE)
+  anexo3 <- RREORGFdataR::RGFdata(cod.ibge = 52, ano = 2023, poder = 'E', periodo = 3, anexo = 1, simplificado = FALSE)
+
+  testthat::expect_s3_class(anexo1, "data.frame")
+  testthat::expect_s3_class(anexo2, "data.frame")
+  testthat::expect_s3_class(anexo3, "data.frame")
+
+})
+
+
+test_that("check number of column", {
+
+  anexo1 <- RREORGFdataR::RGFdata(cod.ibge = 52, ano = 2023, poder = 'E', periodo = 1, anexo = 1, simplificado = FALSE)
+  anexo2 <- RREORGFdataR::RGFdata(cod.ibge = 52, ano = 2023, poder = 'E', periodo = 2, anexo = 1, simplificado = FALSE)
+  anexo3 <- RREORGFdataR::RGFdata(cod.ibge = 52, ano = 2023, poder = 'E', periodo = 3, anexo = 1, simplificado = FALSE)
+
+  testthat::expect_equal(ncol(anexo1), 17)
+  testthat::expect_equal(ncol(anexo2), 17)
+  testthat::expect_equal(ncol(anexo3), 17)
+
+})
